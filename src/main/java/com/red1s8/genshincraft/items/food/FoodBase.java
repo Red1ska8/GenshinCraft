b3e217e4cc2bbe97dc7a27f02b3532d2a31baf58
Red1ska <denisbuga505@gmail.com>
2022-02-04 14:05:12 +0200

    Applied radish and radish balls as a food

--- src/main/java/com/red1s8/genshincraft/items/food/FoodBase.java
+++ src/main/java/com/red1s8/genshincraft/items/food/FoodBase.java
@@ -1,6 +1,11 @@
 package com.red1s8.genshincraft.items.food;
 
 import com.red1s8.genshincraft.GenshinCraft;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -8,11 +13,21 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-public class FoodBase {
+import java.util.function.Supplier;
+
+public class FoodBase extends Item{
     public static final DeferredRegister<Item> FOOD = DeferredRegister.create(ForgeRegistries.ITEMS, GenshinCraft.MOD_ID);
 
-    public static final RegistryObject<Item> RADISH = FOOD.register("radish", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
-    public static final RegistryObject<Item> RADISH_BALLS = FOOD.register("radish_balls", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+    public static final RegistryObject<Item> RADISH = FOOD.register("radish",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                    .food(new FoodProperties.Builder().nutrition(2).fast().build())));
+    public static final RegistryObject<Item> RADISH_BALLS = FOOD.register("radish_balls",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)
+                    .food(new FoodProperties.Builder().effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST,200,0),1f).build())));
+
+    public FoodBase(Properties pProperties) {
+        super(pProperties);
+    }
 
     public static void register(IEventBus eventBus){
         FOOD.register(eventBus);
